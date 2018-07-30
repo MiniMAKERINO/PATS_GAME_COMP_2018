394e0b599b5afef3cb055abf517926f4b7013dde
MiniMAKERINO <jd_mak@yahoo.com.au>
2018-07-31 01:05:26 +1000

    Need a lot more

---
+++ 2018 Game Competition/Assets/Scripts/Shift.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shift : MonoBehaviour {
+
+    public GameObject TPobject;
+    public GameObject TPLocation;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player") ;
+        {
+            TPobject.transform.position = TPLocation.transform.position;
+        }
+    }
+}
