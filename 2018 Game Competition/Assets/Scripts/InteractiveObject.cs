1d865920df4504db2f4b2527945f748a6f46cc58
MiniMAKERINO <jd_mak@yahoo.com.au>
2018-07-31 16:14:32 +1000

    2 Nights to finish all

--- 2018 Game Competition/Assets/Scripts/InteractiveObject.cs
+++ 2018 Game Competition/Assets/Scripts/InteractiveObject.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class InteractiveObject : MonoBehaviour {
 
+    // SerializeField means that it will show up in inspector
+
     [SerializeField] private Vector3 openPos, closedPos;
 
     [SerializeField] private float animTime;
 
     [SerializeField] private bool isOpen = false;
+
+    private enum MovementType {Rotate, Slide};
+
+    [SerializeField] private MovementType movementType;
 
     private Hashtable iTweenArgs;
 
@@ -20,16 +26,26 @@ public class InteractiveObject : MonoBehaviour {
 	}
 
     public void PerformAction() {
-        {
             if (isOpen) {
                 iTweenArgs["position"] = closedPos;
+                iTweenArgs["rotation"] = closedPos;
             } else {
                 iTweenArgs["position"] = openPos;
+                iTweenArgs["rotation"] = openPos;
+               }
+
+            isOpen = !isOpen; // Inverts the current position
+
+            switch(movementType){
+                // For rotating the object
+                case MovementType.Rotate:
+                    iTween.MoveTo(gameObject, iTweenArgs);
+                    break;
+                // For sliding the object
+                case MovementType.Slide:
+                    iTween.RotateTo(gameObject, iTweenArgs);
+                    break;
             }
 
-            isOpen = !isOpen;
-
-            iTween.MoveTo(gameObject, iTweenArgs);
-        }
-    }
+     }
 }
