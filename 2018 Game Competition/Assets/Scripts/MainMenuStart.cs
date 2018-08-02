b10ca7b4b1d78307ca849049edb42107092f9a55
MiniMAKERINO <jd_mak@yahoo.com.au>
2018-08-02 16:08:42 +1000

    School final updates

---
+++ 2018 Game Competition/Assets/Scripts/MainMenuStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuStart : MonoBehaviour {
+
+
+
+    public void StartGame(){
+
+        SceneManager.LoadScene("House");
+    }
+
+    public void QuitGame(){
+
+        Application.Quit();
+        Debug.Log("Quit Application");
+    }
+}
