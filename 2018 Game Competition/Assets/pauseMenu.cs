7e477fed8b33aa7eed1f5150469579d441a7a7a6
MiniMAKERINO <jd_mak@yahoo.com.au>
2018-08-02 19:39:29 +1000

    Done kiddo

---
+++ 2018 Game Competition/Assets/pauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour {
+
+    public static bool gamePaused = false;
+    public GameObject pauseUI;
+    //public GameObject toDisable;
+
+    // Update is called once per frame
+   // private void Start()
+   // {
+    //    pauseUI.SetActive(false);
+    //}
+
+    private void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+        //toDisable.SetActive(false);
+    }
+
+    public void ToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitGAme()
+    {
+        Application.Quit();
+    }
+}
